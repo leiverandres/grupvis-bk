00dbc128107abf75bb894bcb14e1ff83589003c5
Leiver Andres Campeón <leiverandres04p@hotmail.com>
2017-11-02 19:49:08 -0500

    Complete general schema for API

--- app/src/schema.js
+++ app/src/schema.js
@@ -1,12 +1,46 @@
-const { makeExecutableSchema } = require('graphql-tools');
-const { resolvers } = require('./resolvers');
+const { makeExecutableSchema } = require("graphql-tools");
+const { resolvers } = require("./resolvers");
 
 const typeDefs = `
+type Member {
+  name: String,
+  profileURL: String,
+  rol: String,
+  dedicatedHours: Int
+  startingDate: String,
+  endingDate: String
+}
+
+type Product {
+  category: String,
+  type: String,
+  description: String,
+  isApproved: Boolean
+}
+
 type Group {
-    code: ID!,
-    name: String!,
-    city: String,
-    departament: String
+  code: String!,
+  name: String!,
+  grouplacURL: String,
+  website: String,
+  email: String,
+  city: String,
+  leader: String,
+  classificationDate: String,
+  foundationDate: String,
+  departament: String,
+  city: String,
+  certificationDate: String,
+  clasification: String,
+  knowledgeArea: String,
+  nationalProgramOfScienceAndTechnology: String,
+  secondaryNationalProgramOfScienceAndTechnology: String,
+  strategicPlan: String,
+  institutions: [String],
+  researchLines: [String],
+  applicationFields: [String],
+  members: [Member],
+  products: [Product]
 }
 
 type Query {
