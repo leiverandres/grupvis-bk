1a17e2678592ad240928558516efb8b676657ec6
Leiver Andres Campeón <leiverandres04p@hotmail.com>
2017-11-14 12:37:59 -0500

    WIP

--- app/client/src/components/BarChart.js
+++ app/client/src/components/BarChart.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
-import { graphql } from 'react-apollo';
-import gql from 'graphql-tag';
-import { scaleLinear, scaleBand } from 'd3-scale';
-import { max } from 'd3-array';
-import { select } from 'd3-selection';
-import { axisBottom, axisLeft } from 'd3-axis';
-import './BarChart.css';
+import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
+import { scaleLinear, scaleBand } from "d3-scale";
+import { max } from "d3-array";
+import { select } from "d3-selection";
+import { axisBottom, axisLeft } from "d3-axis";
+import { stack, stackOffsetNone, stackOrderNone } from "d3-shape";
+import "./BarChart.css";
 
 const knowledgeAreaQuery = gql`
   query KnowledgeAreaQuery {
@@ -29,7 +30,8 @@ class BarChartLayout extends Component {
     if (!loading) {
       const countByBigArea = {};
       groups.forEach(groupObj => {
-        const { bigKnowledgeArea, faculty, classification, name } = groupObj;
+        const { bigKnowledgeArea, faculty, name } = groupObj;
+        let classification = groupObj.classification || "Reg";
         if (countByBigArea[bigKnowledgeArea]) {
           // already exist an big area object
           if (countByBigArea[bigKnowledgeArea][classification]) {
@@ -63,23 +65,18 @@ class BarChartLayout extends Component {
         return {
           bigAreaName: item[0],
           classifications: Object.entries(item[1]).map(subitem => {
+            const counter = {};
+            Object.entries(subitem[1]).forEach(entry => {
+              counter[entry[0]] = entry[1].length;
+            });
             return {
-              clasification: subitem[0],
-              countByClassification: Object.entries(
-                subitem[1]
-              ).map(classEntry => {
-                return {
-                  faculty: classEntry[0],
-                  groups: classEntry[1],
-                  total: classEntry[1].length
-                };
-              })
+              classification: subitem[0],
+              ...counter
             };
           })
         };
       });
     }
-    console.log(dataCount);
     return (
       <div>
         {loading ? (
@@ -90,8 +87,8 @@ class BarChartLayout extends Component {
             <BarChart
               dataObj={dataCount[0]}
               size={[500, 500]}
-              width={500}
-              height={400}
+              width={700}
+              height={600}
             />
           </div>
         )}
@@ -110,9 +107,24 @@ class BarChart extends Component {
   }
 
   createBarChart = () => {
-    const { dataObj, width, height } = this.props;
-    const clasifications = Object.keys(dataObj);
-    const counts = Object.values(dataObj);
+    const { dataObj: { classifications }, width, height } = this.props;
+
+    const faculties = Array.from(
+      classifications.reduce((acumSet, curItem) => {
+        Object.keys(curItem)
+          .filter(key => key !== "classification")
+          .forEach(item => {
+            acumSet.add(item);
+          });
+        return acumSet;
+      }, new Set())
+    );
+    const stackData = stack()
+      .keys(faculties)
+      .order(stackOrderNone)
+      .offset(stackOffsetNone);
+    const series = stackData(classifications);
+    console.log(series);
     const margin = {
       top: 20,
       left: 50,
@@ -125,61 +137,64 @@ class BarChart extends Component {
     const svg = select(this.nodeRef);
 
     svg
-      .append('g')
-      .attr('class', 'graph')
-      .attr('transform', `translate(${margin.left}, ${margin.top})`);
+      .append("g")
+      .attr("class", "graph")
+      .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
     const xScale = scaleBand()
       .range([0, chartWidth])
-      .domain(clasifications);
+      .domain(classifications.map(c => c.clasification));
 
     const yScale = scaleLinear()
       .range([chartHeight, 0])
-      .domain([0, max(counts)]);
+      .domain([0, 20]);
 
+    // AXES
     svg
-      .append('g')
-      .attr('class', 'axis--x')
+      .append("g")
+      .attr("class", "axis--x")
       .attr(
-        'transform',
+        "transform",
         `translate(${margin.left}, ${chartHeight + margin.top})`
       )
       .call(axisBottom(xScale));
 
     svg
-      .append('text')
-      .attr('x', chartWidth / 2)
-      .attr('y', chartHeight + 3 * margin.top)
-      .attr('text-achor', 'middle')
-      .attr('fill', 'initial')
-      .text('Clasificación');
-
-    svg
-      .append('text')
-      .attr('transform', 'rotate(-90)')
-      .attr('y', margin.left / 4)
-      .attr('x', -chartHeight / 2 + margin.top)
-      .attr('dy', '0.5em')
-      .attr('class', 'label')
-      .text('Cantidad de grupos');
-
-    svg
-      .append('g')
-      .attr('class', 'axis--y')
-      .attr('transform', `translate(${margin.left}, ${margin.top})`)
+      .append("g")
+      .attr("class", "axis--y")
+      .attr("transform", `translate(${margin.left}, ${margin.top})`)
       .call(axisLeft(yScale).ticks(10));
 
+    // LABELS
     svg
-      .select('.graph')
-      .selectAll('.bar')
-      .data(counts)
+      .append("text")
+      .attr("x", chartWidth / 2)
+      .attr("y", chartHeight + 3 * margin.top)
+      .attr("text-achor", "middle")
+      .attr("fill", "initial")
+      .text("Clasificación");
+
+    svg
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", margin.left / 4)
+      .attr("x", -chartHeight / 2 + margin.top)
+      .attr("dy", "0.5em")
+      .attr("class", "label")
+      .text("Cantidad de grupos");
+
+    // BARS
+    svg
+      .select(".graph")
+      .selectAll(".bar")
+      .data(classifications, d => false)
       .enter()
-      .append('rect')
-      .attr('class', 'bar')
-      .attr('x', (d, i) => xScale(clasifications[i]) + barMargin)
-      .attr('y', d => yScale(d))
-      .attr('width', xScale.bandwidth() - barMargin)
-      .attr('height', d => chartHeight - yScale(d));
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", (d, i) => xScale(classifications[i]) + barMargin)
+      .attr("y", d => yScale(d))
+      .attr("width", xScale.bandwidth() - barMargin)
+      .attr("height", d => chartHeight - yScale(d));
   };
 
   render() {
@@ -191,7 +206,7 @@ class BarChart extends Component {
           ref={svgNode => (this.nodeRef = svgNode)}
           width={width}
           height={height}
-          style={{ border: 'solid 3px black' }}
+          style={{ border: "solid 3px black" }}
         />
       </div>
     );
