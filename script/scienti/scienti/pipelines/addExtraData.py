2fd1c05365875a8d0cd5c29b8de9e13db2aa2e6b
Leiver Andres Campeón <leiverandres04p@hotmail.com>
2017-11-23 12:38:07 -0500

    Divde classification 2015 and 2017

--- script/scienti/scienti/pipelines/addExtraData.py
+++ script/scienti/scienti/pipelines/addExtraData.py
@@ -4,14 +4,19 @@ import os
 
 
 class AddExtraDataPipeline(object):
-    file_name = 'grupos_utp.xls'
+    ## info provided by UTP
+    utp_file_name = 'grupos_utp.xls'
+    ## info published by Colciencias
+    preliminary_result_file_name = "resultados-preliminares-grupos-conv_781.xlsx"
 
     def open_spider(self, spider):
         '''
         Read the file where the data will be extracted from
         '''
-        self.dt = pd.read_excel(
-            os.path.join(os.getcwd(), 'data', self.file_name))
+        utp_path = os.path.join('data', self.utp_file_name)
+        colci_path = os.path.join('data', self.preliminary_result_file_name)
+        self.dt_utp = pd.read_excel(utp_path)
+        self.dt_colci = pd.read_excel(colci_path)
 
     def close_spider(self, spider):
         pass
@@ -21,8 +26,18 @@ class AddExtraDataPipeline(object):
         Handle each item, extracting extra info from excel file and
         adding it to the item dictionary
         '''
-        query_code = item['code']
-        row = self.dt.loc[self.dt['CODIGOGRUPO'] == query_code]
-        item['faculty'] = row['FACULTAD'].values[0]
-        item['dependency'] = row['DEPENDENCIA'].values[0]
+        querycode = item['code']
+
+        ## Extracting data from utp data
+        row_utp = self.dt_utp.loc[self.dt_utp['CODIGOGRUPO'] == querycode]
+        item['faculty'] = row_utp['FACULTAD'].values[0]
+        item['dependency'] = row_utp['DEPENDENCIA'].values[0]
+
+        # Extracting data from utp
+        row_c = self.dt_colci[self.dt_colci['CÓDIGO DE GRUPO'] == querycode]
+        if not row_c.empty:
+            item['classification2017'] = row_c[
+                'CLASIFICACIÓN DEL GRUPO'].values[0]
+        else:
+            item['classification2017'] = ''
         return item
