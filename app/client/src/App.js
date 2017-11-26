a0c93fa60f066b428c08a8a99ef4d62c1e92d5c1
Leiver Andres Campeón <leiverandres04p@hotmail.com>
2017-11-26 16:49:11 -0500

    Add side bar

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -3,10 +3,12 @@ import { ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { Sidebar, Menu, Icon } from 'semantic-ui-react';
 
 import './App.css';
 import Header from './components/Header';
+import SidebarWrapper from './components/SidebarWrapper';
 import VizList from './components/VizList';
 import BarChartLayout from './visualizations/BigAreaBarChart/BarChartLayout';
 
@@ -16,20 +18,30 @@ const client = new ApolloClient({
 });
 
 class App extends Component {
+  state = { visible: false };
+
+  toggleVisibility = () => {
+    this.setState({ visible: !this.state.visible });
+  };
+
   render() {
+    const { visible } = this.state;
+
     return (
       <ApolloProvider client={client}>
         <BrowserRouter>
-          <div className="App">
-            <Header />
-            <Route exact path="/" component={VizList} />
-            <Route
-              path="/big-area-viz"
-              render={() => (
-                <BarChartLayout size={[500, 500]} width={500} height={500} />
-              )}
-            />
-          </div>
+          <SidebarWrapper visible={visible}>
+            <div className="App" style={{ heigth: '100%' }}>
+              <Header handleToggleVisibility={this.toggleVisibility} />
+              <Route exact path="/" component={VizList} />
+              <Route
+                path="/big-area-viz"
+                render={() => (
+                  <BarChartLayout size={[500, 500]} width={500} height={500} />
+                )}
+              />
+            </div>
+          </SidebarWrapper>
         </BrowserRouter>
       </ApolloProvider>
     );
---
+++ app/client/src/components/SidebarWrapper.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Sidebar, Menu, Icon } from 'semantic-ui-react';
+
+class SidebarWrapper extends Component {
+  render() {
+    const { visible } = this.props;
+    return (
+      <Sidebar.Pushable>
+        <Sidebar
+          as={Menu}
+          animation="push"
+          width="thin"
+          visible={visible}
+          icon="labeled"
+          vertical
+          inverted
+        >
+          <Menu.Item name="home" as={Link} to="/">
+            <Icon name="grid layout" />
+            Inicio
+          </Menu.Item>
+          <Menu.Item name="home">
+            <Icon name="table" />
+            Reportes
+          </Menu.Item>
+          <Menu.Item name="home">
+            <Icon name="cubes" />
+            Analisis
+          </Menu.Item>
+          <Menu.Item name="gamepad">
+            <Menu.Header>Enlaces de interes</Menu.Header>
+            <Menu.Menu>
+              <Menu.Item href="http://sirius.utp.edu.co/" target="_blank">
+                Grupo de investigación Sirius
+              </Menu.Item>
+              <Menu.Item
+                href="http://www.utp.edu.co/vicerrectoria/investigaciones/"
+                target="_blank"
+              >
+                Vicerrectoría de Investigaciones, Innovación y Extensión
+              </Menu.Item>
+            </Menu.Menu>
+          </Menu.Item>
+        </Sidebar>
+        <Sidebar.Pusher>{this.props.children}</Sidebar.Pusher>
+      </Sidebar.Pushable>
+    );
+  }
+}
+
+export default SidebarWrapper;
