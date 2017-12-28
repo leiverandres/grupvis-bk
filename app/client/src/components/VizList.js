7e65ab1e742f123a30410a054114e0777ce47287
Leiver Andres Campeón <leiverandres04p@hotmail.com>
2017-12-28 11:02:00 -0500

    ready for deploy

--- app/client/src/components/VizList.js
+++ app/client/src/components/VizList.js
@@ -22,13 +22,7 @@ const cardsArray = [
     img: 'https://static.pexels.com/photos/672802/pexels-photo-672802.jpeg',
     header: 'Cantidad de grupos de investigación agrupados por clasificación',
     description: 'Descripción corta',
-    linkPath: '/aggregated-groups'
-  },
-  {
-    img: 'https://static.pexels.com/photos/672802/pexels-photo-672802.jpeg',
-    header: 'Grupos de investigación por gran área de conocimiento',
-    description: 'Descripción corta',
-    linkPath: '/big-area-viz'
+    linkPath: '/under-construction'
   }
 ];
 
